ea396f2929d553c36fbdbd2d55da4e4be72982d4
Ihar Tsalka <Ihar_Tsalka@epam.com>
2018-12-05 16:18:54 +0300

    - Add simple implementation of word ngrams (without splitting into char unknown words)

--- bpe/__main__.py
+++ bpe/__main__.py
@@ -9,11 +9,12 @@ def main(corpus_path):
     """ Loads corpus, learns word and BPE vocab, and writes to stdout.  Assumes corpus is
         line-separated text.
     """
-    with open(corpus_path) as infile:
+    with open(corpus_path, encoding="utf8") as infile:
         lines = list(map(str.strip, infile))
 
-    encoder = Encoder(silent=True)
+    encoder = Encoder(silent=True, ngram_max=3)
     encoder.fit(lines)
+    print(encoder.tokenize("There is a leader and he is winner"))
     print(json.dumps(encoder.vocabs_to_dict()))
 
 
